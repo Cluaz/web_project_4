41d100ce18fca2a08240c81502564fc236e5de27
Luanna Cruz <cruz.luanna@hotmail.com>
2022-07-21 20:02:31 -0300

    Java Script

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Project 4: "Around The U.S."
+
+[Link do projeto](https://cluaz.github.io/web_project_4/)
+
+## Overview
+
+Este respositório tem o propósito de aprender a utilizar Java Script num projeto de apenas uma página em que há a indentificação do usuário, que pode ser alterada, bem como cards contendo imagem e nome de lugares, os quais podem ser adicionados, alterados ou excluídos.
+Foi realizado de forma que todos os elementos sejam exibidos corretamente em tamanhos de tela populares (320px, 768px, 1280px). E tenham uma transição fluida, sem quebras de layout, apresentando-se bem também nos endpoints: 470px, 665px, 999px, 1100px e 1500px.
+
+## Técnicas utilizadas
+
+* Metodologia BEM, organização Flat BEM
+* HTML semântico
+* Estilização CSS (flexbox, grid, posicionamento de imagens e links)
+* Java Script
+* [Link to the project in Figma](https://www.figma.com/file/XfB6BSINvliub43JgKza1e/WEB.-Sprint-4.-Around-The-U.S.-desktop-%2B-mobile-pt?node-id=1%3A147)
--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,49 @@
 // Vamos descobrir o formulário no DOM
-let formElement = // Use o método querySelector()
+let popup = document.querySelector(".popup");
+let profileEdit = document.querySelector(".button_edit");
+let profileToggle = document.querySelector(".button_toggle");
+
+function openPopup() {
+  popup.classList.add("popup_opened");
+}
+
+profileEdit.addEventListener("click", openPopup);
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+profileToggle.addEventListener("click", closePopup);
+
+let formElement = document.querySelector(".popup__container"); // Use o método querySelector()
 
 // Em seguida vem o handler do submit
 // ainda não vai enviar para lugara nenhum
+let profileFormSubmit = formElement.querySelector(".button_save");
 
 // Observe que o nome da função começa com um verbo
 // e descreve exatamente o que a função faz
 function handleProfileFormSubmit(evt) {
-    // Esta linha impede o navegador 
-    // de enviar o formulário da forma padrão.
-    evt.preventDefault();
-    // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
-    // Explicaremos em mais detalhes posteriormente.
+  // Esta linha impede o navegador
+  // de enviar o formulário da forma padrão.
+  evt.preventDefault();
+  // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
+  // Explicaremos em mais detalhes posteriormente.
 
-    // Vamos encontrar os campos de formulário do DOM
-    let nameInput = // Use querySelector()
-    let jobInput = // Use querySelector()
+  // Vamos encontrar os campos de formulário do DOM
+  let nameInput = formElement.querySelector("#name"); // Use querySelector()
+  let jobInput = formElement.querySelector("#about"); // Use querySelector()
 
-    // Pegue os valores de cada campo do valor de propriedade correspondente
+  // Selecione elementos onde os valores de campo serão inseridos
+  let profileName = formElement.querySelector(".profile__name");
+  let profileAbout = formElement.querySelector(".profile__about");
 
-    // Selecione elementos onde os valores de campo serão inseridos
-
-    // Insira novos valores usando textContent
-    // propriedade do método querySelector()
+  // Insira novos valores usando textContent
+  // propriedade do método querySelector()
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = jobInput.value;
 }
 
 // Conecte o handler ao formulário:
 // ele vai observar o evento de submit
-formElement.addEventListener('submit', handleProfileFormSubmit);
+formElement.addEventListener("submit", handleProfileFormSubmit + closePopup);
