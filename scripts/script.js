80007941218a0f12ba2620e06d33c88f8b856e66
Luanna Cruz <cruz.luanna@hotmail.com>
2022-08-12 18:06:06 -0300

    fix bugs, set events Java Script

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,22 @@
-const popup = document.querySelector(".popup");
+const popupProfile = document.querySelector("#edit-profile");
 const profileEdit = document.querySelector(".button_edit");
-const profileToggle = document.querySelector(".button_toggle");
+const popupProfileToggle = document.querySelector("#profile-toggle");
 
-function togglePopup() {
-  popup.classList.toggle("popup_opened");
+function toggleProfilePopup() {
+  popupProfile.classList.toggle("popup_opened");
 }
 
-profileEdit.addEventListener("click", togglePopup);
+profileEdit.addEventListener("click", toggleProfilePopup);
 
-profileToggle.addEventListener("click", togglePopup);
+popupProfileToggle.addEventListener("click", toggleProfilePopup);
 
-const formElement = document.querySelector(".popup__container");
+const formProfileElement = document.querySelector("#edit-profile-form");
+const profileFormSubmit = formProfileElement.querySelector("#save-profile");
 
-const profileFormSubmit = formElement.querySelector(".button_save");
-
-profileFormSubmit.onclick = togglePopup;
+profileFormSubmit.onclick = toggleProfilePopup;
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
-  // Explicaremos em mais detalhes posteriormente.
 
   const nameInput = document.querySelector("#name");
   const aboutInput = document.querySelector("#about");
@@ -31,4 +28,113 @@ function handleProfileFormSubmit(evt) {
   profileAbout.textContent = aboutInput.value;
 }
 
-formElement.addEventListener("submit", handleProfileFormSubmit);
+formProfileElement.addEventListener("submit", handleProfileFormSubmit);
+
+const cardsContainer = document.querySelector(".cards");
+const cardTemplate = document.querySelector("#card-template");
+
+const popupAddCard = document.querySelector("#popup-add-card");
+const cardAdd = document.querySelector(".button_add");
+const popupAddCardToggle = document.querySelector("#add-card-toggle");
+const cardFormSubmit = document.querySelector("#add-card-form");
+
+function toggleAddCardPopup() {
+  popupAddCard.classList.toggle("popup_opened");
+}
+
+cardAdd.addEventListener("click", toggleAddCardPopup);
+popupAddCardToggle.addEventListener("click", toggleAddCardPopup);
+
+function addCard(imageInput, titleInput) {
+  const cardElement = cardTemplate.content.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardLike = cardElement.querySelector(".button_like");
+  const cardDelete = cardElement.querySelector(".button_delete");
+  const popupCardToggle = cardElement.querySelector("#card-toggle");
+
+  cardImage.src = imageInput;
+  cardImage.alt = titleInput;
+
+  cardElement.querySelector(".card__title").textContent = titleInput;
+  cardElement.querySelector(".popup__image").src = imageInput;
+  cardElement.querySelector(".popup__title").textContent = titleInput;
+
+  cardLike.addEventListener("click", likeCard);
+  cardImage.addEventListener("click", handleCardPopup);
+  popupCardToggle.addEventListener("click", handleCardPopup);
+  cardDelete.addEventListener("click", deleteCard);
+
+  return cardElement;
+}
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+
+  const titleInput = document.querySelector("#title").value;
+  const imageInput = document.querySelector("#image").value;
+  cardsContainer.prepend(addCard(imageInput, titleInput));
+  document.querySelector("#title").value = "";
+  document.querySelector("#image").value = "";
+
+  toggleAddCardPopup();
+}
+
+cardFormSubmit.addEventListener("submit", handleCardFormSubmit);
+
+function toggleCardPopup() {
+  cardPopup.classList.toggle("popup_opened");
+}
+
+function handleCardPopup(evt) {
+  evt.preventDefault();
+  const cardElement = evt.target.closest(".card");
+  const cardPopup = cardElement.querySelector("#popup-card");
+  cardPopup.classList.toggle("popup_opened");
+}
+
+const initialCards = [
+  {
+    title: "Vale de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    title: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    title: "Montanhas Carecas",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    title: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    title: "Parque Nacional da Vanoise ",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    title: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+initialCards.forEach((item) => {
+  cardsContainer.append(addCard(item.link, item.title));
+});
+
+function likeCard(evt) {
+  const likeImage = evt.currentTarget.querySelector(".button__image_like");
+  const srcImage = likeImage.getAttribute("src");
+
+  srcImage.includes("./images/like.svg")
+    ? likeImage.setAttribute("src", "../images/like_active.svg")
+    : likeImage.setAttribute("src", "../images/like.svg");
+
+  return likeImage;
+}
+
+function deleteCard(evt) {
+  const cardElement = evt.target.closest(".card");
+  cardElement.remove();
+}
